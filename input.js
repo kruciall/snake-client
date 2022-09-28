7cee6e602d69e20c26e54d13915e0db9010683fa
Vinson Wong <vinson.wong93@gmail.com>
2022-09-28 15:51:28 -0400

    implemented wasd keys to move the snake

--- input.js
+++ input.js
@@ -1,20 +1,33 @@
-const setupInput = function () {
+const { stdin } = require("process");
+
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on('data', key => {
     handleUserInput(key);
+    return stdin;
   });
 
-  return stdin;
-};
 
-const handleUserInput = (key) => {
-  const stdout = process.stdout;
-  if (key === '\u0003') {
-    process.exit();
-  }
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    } else if (key === 'w') {
+      connection.write("Move: up");
+    } else if (key === 'a') {
+      connection.write("Move: left");
+    } else if (key === 'd') {
+      connection.write("Move: right");
+    } else if (key === 's') {
+      connection.write("Move: down");
+    }
+  };
+
 };
 
 module.exports = { setupInput };
